edf7c01e51da29849adb6975f1d618ab87ff1e4f
anketmanoj <68798781+anketmanoj@users.noreply.github.com>
2022-09-20 07:43:51 +0900

    setting up init routes

---
+++ lib/router.dart
@@ -0,0 +1,21 @@
+import 'package:amazon_clone_new/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AuthScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => AuthScreen(),
+      );
+    default:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => Scaffold(
+          body: Center(
+            child: Text("ERROR 404"),
+          ),
+        ),
+      );
+  }
+}
